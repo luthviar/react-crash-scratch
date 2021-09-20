dc921d852547226f81d4870fa2496c11e83d03d3
Luthfi Abdurrahim <luthfi@learn-quran.co>
2021-09-20 18:34:47 +0700

    ninitate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+//importing components
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Luthfi's Todo List</h1>
+      </header>      
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
